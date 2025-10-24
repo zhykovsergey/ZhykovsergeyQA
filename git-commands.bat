c967770ae8644bbb1301e84c1f18f78b2eb7695d
Sergey <zhykovsergey@gmail.com>
2025-10-24 14:53:48 +0300

    fix: update GitHub Actions to use v4 artifacts

---
+++ git-commands.bat
@@ -0,0 +1,29 @@
+@echo off
+REM Простой скрипт для работы с Git
+
+if "%1"=="add" (
+    "C:\Program Files\Git\bin\git.exe" add .
+    goto :eof
+)
+
+if "%1"=="commit" (
+    "C:\Program Files\Git\bin\git.exe" commit -m "fix: update GitHub Actions to use v4 artifacts"
+    goto :eof
+)
+
+if "%1"=="push" (
+    "C:\Program Files\Git\bin\git.exe" push origin main
+    goto :eof
+)
+
+if "%1"=="status" (
+    "C:\Program Files\Git\bin\git.exe" status
+    goto :eof
+)
+
+if "%1"=="remote" (
+    "C:\Program Files\Git\bin\git.exe" remote add origin %2
+    goto :eof
+)
+
+echo Usage: git-commands.bat [add|commit|push|status|remote URL]
